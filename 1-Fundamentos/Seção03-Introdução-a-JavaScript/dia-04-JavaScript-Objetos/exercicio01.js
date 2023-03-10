cef281d07b73d02b5b7a18af202ed09846d93b88
iesley-ferreira <iesley_ferreira@hotmail.com>
2023-03-09 23:48:24 -0300

    exercicios dias 01 e alguns dia 02

--- 1-Fundamentos/Seção03-Introdução-a-JavaScript/dia-04-JavaScript-Objetos/exercicio01.js
+++ 1-Fundamentos/Seção03-Introdução-a-JavaScript/dia-04-JavaScript-Objetos/exercicio01.js
@@ -1,15 +1,58 @@
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3
-    }
-};
-
-player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
-
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
-console.log('A jogadora' + player.name + ' ' + player.lastName + 'foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+    let order = {
+        name: 'Rafael Andrade',
+        phoneNumber: '11-98763-1416',
+        address: {
+        street: 'Rua das Flores',
+        number: '389',
+        apartment: '701',
+        },
+        order: {
+        pizza: {
+            marguerita: {
+            amount: 1,
+            price: 25,
+            },
+            pepperoni: {
+            amount: 1,
+            price: 20,
+            },
+        },
+        drinks: {
+            coke: {
+            type: 'Coca-Cola Zero',
+            price: 10,
+            amount: 1,
+            },
+        },
+        delivery: {
+            deliveryPerson: 'Ana Silveira',
+            price: 5,
+        },
+        },
+        payment: {
+        total: 60,
+        },
+    };
+  
+  function customerInfo(order) {
+    let deliveryPerson = order.order.delivery.deliveryPerson;
+    let customerName = order['name'];
+    let customerPhone = order['phoneNumber'];
+    let street = order['address'].street;
+    let number = order['address'].number;
+    let apartment = order['address'].apartment;
+    return `Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, N°. ${number} AP: ${apartment}`
+}
+  
+  customerInfo(order);
+console.log(customerInfo(order));
+  
+  function orderModifier(order) {
+    let otherBuyer = order.name = 'Luiz Silva';
+    let newTotal = order.payment.total = '50';
+    let pizzas = Object.keys(order.order.pizza);
+    let drinks = order.order.drinks.coke.type;
+    console.log(`Olá ${otherBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${newTotal},00`);
+  }
+  
+  orderModifier(order);
