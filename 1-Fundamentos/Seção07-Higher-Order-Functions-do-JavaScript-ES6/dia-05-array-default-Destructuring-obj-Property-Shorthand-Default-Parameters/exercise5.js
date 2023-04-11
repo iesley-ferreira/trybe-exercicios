99b8e91b3393ab1510a1141a51cc55009f83aa34
iesley-ferreira <iesley_ferreira@hotmail.com>
2023-04-10 23:27:31 -0300

    resolve exercicios dia 5 sessao 7

---
+++ 1-Fundamentos/Seção07-Higher-Order-Functions-do-JavaScript-ES6/dia-05-array-default-Destructuring-obj-Property-Shorthand-Default-Parameters/exercise5.js
@@ -0,0 +1,20 @@
+const moreStudents = [
+  'Chris', 
+  'Ahmad', 
+  'Antigoni', 
+  'Chris',
+  'Sam',
+];
+
+const [student1, [student2, student3], [student4, student5]] = moreStudents;
+
+console.log(student1, student2, student3, student4, student5);
+//
+//================================================================================================================================
+//
+const firstArray = [3, 5, 7];
+
+
+
+
+
